3e5691941bec3db517d003f498b921b82476b6c7
Vicente Sousa <vicente.sousa@usercentrics-ext.com>
2026-02-01 11:23:41 +0000

    main menu

--- assets/scripts/ui/bucket_list.gd
+++ assets/scripts/ui/bucket_list.gd
@@ -3,12 +3,12 @@ class_name BucketList extends VBoxContainer
 @export var slots: Array[BucketListItem]
 
 func check_task(task_name: String) -> void:
-    var idx := find_task(task_name)
-    if idx != -1:
-        slots[idx].check()
+	var idx := find_task(task_name)
+	if idx != -1:
+		slots[idx].check()
 
 func find_task(task_name: String) -> int:
-    for i in slots.size():
-        if slots[i].task_name == task_name:
-            return i
-    return -1
+	for i in slots.size():
+		if slots[i].task_name == task_name:
+			return i
+	return -1
